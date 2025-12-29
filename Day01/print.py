756b7ae1cf6b409521642a6049f4545ded018790
phoenixarjuna <venkat.anampudi@gmail.com>
2025-12-29 14:17:43 -0500

    working with variables

---
+++ Day01/print.py
@@ -0,0 +1,8 @@
+# print ("Notes from Day 1")
+# print     ("The print statement is used to output strings")
+# print          ("Strings are strings of characters")
+# print              ("String Concatenation is done with the + sign")
+# print                      ("New lines can be created with a \ and the letter n")
+
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes.\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
---
+++ Day01/task.py
@@ -0,0 +1,2 @@
+input         ("What is your name?")
+input("What is your age?")
